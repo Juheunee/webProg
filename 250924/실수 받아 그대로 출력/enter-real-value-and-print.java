2e9878bfd4da77ec094b84318ef3dd76ba88999d
Juheun <124763454+Juheunee@users.noreply.github.com>
2025-09-24 18:16:07 +0900

    Title: 실수 받아 그대로 출력, Time: 140ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250924/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        double getInput = scanner.nextDouble();
+
+        System.out.printf("%.2f%n", getInput);
+    }
+}
